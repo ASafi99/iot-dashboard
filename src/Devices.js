492c16510e8567fd7338cde4b2f101755c8f536b
A.Safi <samsafi52@hotmail.com>
2020-12-29 15:10:30 +0000

    started device form

--- src/Devices.js
+++ src/Devices.js
@@ -1,41 +1,52 @@
-import React, { Component} from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal } from './Modal';
+import styled from 'styled-components';
 
-class Devices extends Component {
+const title = {
+    right: "500px",
+    marginTop: "-150px",
+    position: "absolute",
+    textAlign: "center",
+    top: "50%",
+    width: "100%",
+    fontSize:"30px",
 
-    
-  
-    render (){
+}
 
-        const title = {
-            right: "500px",
-            marginTop: "-150px",
-            position: "absolute",
-            textAlign: "center",
-            top: "50%",
-            width: "100%",
-            fontSize:"30px",
+const pos = {
+    right: "100px",
+    marginTop: "150px",
+    position: "absolute",
+    textAlign: "center",
+     
+}
 
-        }
 
-        const pos = {
-            right: "100px",
-            marginTop: "150px",
-            position: "absolute",
-            textAlign: "center",
-             
-        }
 
-        return(
-            <div>
+function Devices (){
+
+   
+        
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+      setShowModal(prev => !prev);
+
+    }
+
+       return(
+
+            <>
+           
             <h1 style = {title} >Devices </h1>
 
-            <Button variant="primary" style = {pos}>Primary</Button>
+            <Button onClick={openModal} variant="primary" style = {pos}>Create Device</Button>
+            <Modal showModal={showModal} setShowModal={setShowModal} />
     
-        </div>
+            </>
         )
     }   
-}
 
 export default Devices
